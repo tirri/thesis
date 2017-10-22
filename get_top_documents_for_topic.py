58ea4c39c0a3631aa5d96dc55b49514b8a3cd5c7
tirri <sorsa.tiia@gmail.com>
2017-10-22 16:35:57 +0300

    added files

--- get_top_documents_for_topic.py
+++ get_top_documents_for_topic.py
@@ -10,7 +10,7 @@ from gensim import corpora, models
 
 def main():
 
-    model_version = 30 # which version of the model are we investigating? 30, 100 or 200 topics?
+    model_version = 200 # which version of the model are we investigating? 30, 100 or 200 topics?
 
     #load the respecting corpus, gensim-dictionary and lda-model:
     corpus = corpora.MmCorpus('/Users/tirri/LDA/%stopics.mm' % (model_version))
@@ -18,26 +18,26 @@ def main():
     model = models.LdaModel.load('/Users/tirri/LDA/%stopics.model' % (model_version))
 
     # path_to_originals = '/Users/tirri/LDA/bows4/'
-    topic_n = 12 # which topic are we investigating?
+    topic_n = 3 # which topic are we investigating?
 
     documents_for_topic = get_documents_for_topic(topic_n, model, dictionary, corpus)
+    print(documents_for_topic)
 
 
 def get_document(n_of_document):
-    with open('/Volumes/Transcend/Documents/GRADU/Tiedostot/uudet_lemmat/file%s.txt.lemma' % (n_of_document)) as file:
+    with open('/Users/tirri/LDA/bows/file%s.txt.lemma' % (n_of_document)) as file:
         text = file.readline()
         document = [word for word in text.split()]
     return document
 
 def get_topics_for_document(document, dictionary, model):
     bow = dictionary.doc2bow(document)
-    topics = model.get_document_topics(bow, minimum_probability = 0.4)
+    topics = model.get_document_topics(bow, minimum_probability = 0.25)
     return topics
 
 def get_documents_for_topic(topic_n, model, dictionary, document_matrix):
-
+    documents_for_topic = []  # a list of doc_id, topic_probability -tuples
     for doc_id in range(1, len(document_matrix)):
-        documents_for_topic = [] # a list of doc_id, topic_probability -tuples
         document = get_document(doc_id)
         topics_for_document = get_topics_for_document(document, dictionary, model)
 
@@ -45,6 +45,8 @@ def get_documents_for_topic(topic_n, model, dictionary, document_matrix):
             if topic[0] == topic_n:
                 tuple = (doc_id, topic[1])
                 documents_for_topic.append(tuple)
-                print(documents_for_topic)
+                # print(documents_for_topic)
+
+    return documents_for_topic
 
 main()
