d02448301e4ad6fbe312ba86a09e1f0e87bd9ccf
tirri <sorsa.tiia@gmail.com>
2019-04-25 18:43:31 +0300

    documentation

--- lda_of_messages2.py
+++ lda_of_messages2.py
@@ -1,13 +1,16 @@
+# The main .py -file that models the data into topics
+
 import numpy
 from gensim import corpora, models
 import logging
 
+# It is nice to know what the model is doing, this will give some information:
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
     level=logging.INFO)
 
 def main():
     model = get_model()
-    topics = (model.print_topics(200, 10))
+    topics = (model.print_topics(200, 15))
     with open('200topics.txt', 'w') as file:
         file.write(str(topics))
 
@@ -26,7 +29,7 @@ def get_model():
     dictionary = get_dictionary()
     print(dictionary)
     model = models.LdaModel(corpus, id2word=dictionary, num_topics=200, minimum_probability=0.01, alpha='auto', eta='auto', iterations = 1000, eval_every=10,
-                            random_state=numpy.random.RandomState(10))
+                            random_state=numpy.random.RandomState(10)) # define random state or each run gives different result
     model.save('/Users/tirri/LDA/200topics.model')
     return model
 
